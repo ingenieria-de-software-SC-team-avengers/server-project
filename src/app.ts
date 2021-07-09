8180a589ff66e3ddeb2582c3cd47af3f5fbf1bb0
Cristhian Vargas <cristhianingsis@gmail.com>
2021-07-08 22:12:11 -0400

    prueba

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,13 @@ import express from "express";
 
 export default class Server{
     public app: express.Application;
-    public port: number;
 
     constructor(){
         this.app = express();
-        this.port = 3000;
     }
 
     start(callback: any){
-        this.app.listen(this.port || process.env.PORT || 3000, callback);
+        this.app.listen(callback);
     }
 }
 
