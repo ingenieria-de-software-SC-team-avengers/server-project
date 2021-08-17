3fed1b5bb6427dba2582f0b829908126865426bc
Cristhian Vargas <cristhianingsis@gmail.com>
2021-08-17 02:35:33 -0400

    grafica

--- src/controllers/dataSensorsController.ts
+++ src/controllers/dataSensorsController.ts
@@ -48,3 +48,60 @@ export const getAllTemperatures = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const getDataGrafica = async(req: Request, res: Response) => {
+  try {
+    let valores: number[] = [0];
+    const { username } = req.body;
+    const userResponse: QueryResult = await pool.query(
+      "SELECT users.id FROM users WHERE users.username = $1",
+      [username]
+    );
+    const idUser = userResponse.rows[0].id;
+    const temperatures: QueryResult = await pool.query(
+      "SELECT datasensors.valor,datasensors.fecha FROM datasensors WHERE datasensors.iduser = $1",
+      [idUser]
+    );
+    const data = temperatures.rows;
+    data.forEach(element => {
+      valores.push(element.valor);
+    });
+    return res.status(200).json([
+      {data: valores, label: 'Temperaturas'}
+    ]);
+    console.log(valores);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error de datos"
+    });
+  }
+}
+
+export const getDateGrafica = async(req: Request, res: Response) => {
+  try {
+    let valores: string[] = ['0'];
+    const { username } = req.body;
+    const userResponse: QueryResult = await pool.query(
+      "SELECT users.id FROM users WHERE users.username = $1",
+      [username]
+    );
+    const idUser = userResponse.rows[0].id;
+    const temperatures: QueryResult = await pool.query(
+      "SELECT datasensors.valor,datasensors.fecha FROM datasensors WHERE datasensors.iduser = $1",
+      [idUser]
+    );
+    const data = temperatures.rows;
+    data.forEach(element => {
+      let dateT = new Date(element.fecha);
+      let fecha: string = dateT.getFullYear()+'/'+dateT.getMonth()+'/'+dateT.getDate()+' '+dateT.getHours()+':'+dateT.getMinutes();
+      //console.log(dateT.getFullYear());
+      valores.push(fecha);
+    });
+    console.log(valores);
+    return res.status(200).json(valores);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error de datos"
+    });
+  }
+}
--- src/routes/dataSensorsRoutes.ts
+++ src/routes/dataSensorsRoutes.ts
@@ -5,5 +5,7 @@ const router = Router();
 
 router.post('/dataRegister', dataController.registerTemperature);
 router.post('/getTemperatures', dataController.getAllTemperatures);
+router.post('/getDataGrafic', dataController.getDataGrafica);
+router.post('/getDateGrafic', dataController.getDateGrafica);
 
 export default router;
